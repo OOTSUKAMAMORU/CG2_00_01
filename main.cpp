ca49f240ee283b7a354436db99cb6e2a89d1aec8
OOTSUKAMAMORU <k024g0018@g.neec.ac.jp>
2026-01-05 15:30:11 +0900

    GE3 01_05

--- main.cpp
+++ main.cpp
@@ -374,6 +374,7 @@ ID3D12Resource* CreateBufferResource(ID3D12Device* device, size_t sizeInBytes)
 
 	//バッファリソース。テクスチャの場合はまた別の設定をする
 	vertexResourceDesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	
 	//vertexResourceDesc.Width = sizeof(Vector4) * 3;
 	vertexResourceDesc.Width = sizeInBytes;
 
